58087c72b55b320f983f86dfc4c33a8eb6d6e45f
arisbol <arisbol7@gmail.com>
2022-05-29 21:18:16 +0300

    update 3

--- UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/ReportList.kt
+++ UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/ReportList.kt
@@ -1,7 +1,14 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class ReportList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +17,10 @@ class ReportList : AppCompatActivity() {
 
         val actionBar = supportActionBar
 
-            actionBar!!.title = "Report List"
+        actionBar!!.title = "Report List"
 
-            actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+
     }
 }
