b6464c818d03056a636fc91810486e732c1d893c
arisbol <arisbol7@gmail.com>
2022-05-30 13:17:48 +0300

    update4

--- UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/ReportList.kt
+++ UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/ReportList.kt
@@ -7,8 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
+
+
 
 class ReportList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +23,47 @@ class ReportList : AppCompatActivity() {
         actionBar!!.title = "Report List"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
+        val listView = findViewById<ListView>(R.id.report_listView)
+        listView.adapter = MyCustomAdopter(this)
+    }
+    private class MyCustomAdopter(context: Context): BaseAdapter() {
+        private val mContext: Context
+        private val names = arrayListOf<String>(
+            "Report 0", "Report 1", "Report 2", "Report 3"
+        )
+        init{
+            mContext = context
+        }
+        override fun getView(position: Int, convertView: View?, viewGroup:  ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+            val nametextView = rowMain.findViewById<TextView>(R.id.name_textView)
+            nametextView.text = names.get(position)
+
+            val report_btn = rowMain.findViewById<Button>(R.id.report_btn)
+            report_btn.text = "See report $position"
+
+
+            return rowMain
+
+            //  val textView = TextView(mContext)
+            // textView.text = "Here is"
+            //  return textView
+        }
+
+        override fun getItem(position: Int): Any {
+            return "TEst"
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return names.size
+        }
 
 
     }
-}
+
+    }
