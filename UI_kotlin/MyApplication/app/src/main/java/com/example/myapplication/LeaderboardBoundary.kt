1a92330ab7cc0e088706aba4b91207daf2734ecd
APapathanasiou <alex.goku3000@gmail.com>
2022-05-30 22:26:15 +0300

    leaderboardclass

--- UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/LeaderboardBoundary.kt
+++ UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/LeaderboardBoundary.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
 
 class LeaderboardBoundary : AppCompatActivity() {
+    lateinit var table: TableLayout
+    lateinit var t1: TableRow
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        t1 = TableRow (this)
         setContentView(R.layout.activity_leaderboard_boundary)
     }
 }
