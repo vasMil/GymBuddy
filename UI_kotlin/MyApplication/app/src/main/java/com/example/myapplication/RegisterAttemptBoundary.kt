32db8071d111bf5d15d4da41f60d7cef929c4bae
APapathanasiou <alex.goku3000@gmail.com>
2022-06-04 00:44:44 +0300

    new

--- UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/RegisterAttemptBoundary.kt
+++ UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/RegisterAttemptBoundary.kt
@@ -3,26 +3,28 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
 import android.widget.Toast
 
 class RegisterAttemptBoundary : AppCompatActivity() {
+    private lateinit var confirmButton: Button
+    private lateinit var acceptSwitch: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_attempt_boundary)
     }
 
     private fun handleConfirmation() {
-        if(switch1.isChecked) {
-            Toast.makeText(this@RegisterAttemptBoudary, "Success!",
-                Toast.LENGTH_SHORT)
-                .show()
-            val intent = Intent(this, UserProfile::class.java)
+        val switch = findViewById<Switch>(R.id.switch1)
+        if(switch.isChecked) {
+
+            val intent = Intent(this, LeaderboardBoundary::class.java)
             startActivity(intent)
         }
         else {
-            Toast.makeText(this@RegisterAttemptBoudary, "Failed to upload video!",
-                Toast.LENGTH_SHORT)
-                .show()
+
         }
     }
 
