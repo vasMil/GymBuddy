8d2329d61353d6587db34505f53bf95d9ac72cc1
APapathanasiou <alex.goku3000@gmail.com>
2022-06-04 00:21:58 +0300

    new

--- UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/RegisterAttemptBoundary.kt
+++ UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/RegisterAttemptBoundary.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class RegisterAttemptBoundary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,7 +11,20 @@ class RegisterAttemptBoundary : AppCompatActivity() {
         setContentView(R.layout.activity_register_attempt_boundary)
     }
 
-
+    private fun handleConfirmation() {
+        if(switch1.isChecked) {
+            Toast.makeText(this@RegisterAttemptBoudary, "Success!",
+                Toast.LENGTH_SHORT)
+                .show()
+            val intent = Intent(this, UserProfile::class.java)
+            startActivity(intent)
+        }
+        else {
+            Toast.makeText(this@RegisterAttemptBoudary, "Failed to upload video!",
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
 
 
 }
