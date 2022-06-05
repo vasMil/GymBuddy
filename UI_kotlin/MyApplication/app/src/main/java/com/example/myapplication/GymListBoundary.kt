39a70921d4090a76c221a94125a553f4d6e19136
APapathanasiou <alex.goku3000@gmail.com>
2022-06-05 23:16:03 +0300

    list_view

--- UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/GymListBoundary.kt
+++ UI_kotlin/MyApplication/app/src/main/java/com/example/myapplication/GymListBoundary.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.example.myapplication.ReportList.MyCustomAdopter
 
 class GymListBoundary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gym_list_boundary)
+
+        val actionBar = supportActionBar
+
+        actionBar!!.title = "Gym List"
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        val listView = findViewById<ListView>(R.id.gym_listView)
+        listView.adapter = (this)
     }
 }
